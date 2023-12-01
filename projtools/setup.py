e2cf9ad105293fb578939ce4c82301af7564e01c
Tom Fox <13188249+TomWFox@users.noreply.github.com>
2023-12-01 17:15:30 +0000

    add more files

--- projtools/setup.py
+++ projtools/setup.py
@@ -4,22 +4,6 @@ This file sets up the initial environment.
 """
 import hashlib
 from pathlib import Path
-import pandas as pd
-
-def import_data(filename = "LSOA_Crime_21-23.csv"):
-    """ An example function, returning `a` * 10 + b
-
-    Parameters
-    ----------
-    a : number
-    b : number
-
-    Returns
-    -------
-    res : number
-        Result of ``a * 10 + b``.
-    """
-    return pd.read_csv(filename)
 
 def get_data_hash(filename = "LSOA_Crime_21-23.csv"):
     """ Get the hash of the data file
--- projtools/tests/test_setup.py
+++ projtools/tests/test_setup.py
@@ -3,8 +3,20 @@
 
 from projtools import setup
 
+BOROUGH_CRIME_10_21_HASH = "bcc488bec69c7a6ce7148dd5a05a025498c9488b"
+BOROUGH_CRIME_21_23_HASH = "ea7e1aa0dd0562ba2cc7ef8beffd1911f3c803f7"
+LSOA_CRIME_10_21_HASH = "f250f870067cd90a82b7acefbc3f7ccb2e765166"
 LSOA_CRIME_21_23_HASH = "8057ba79256769903b91b084e1ed0affd8e85277"
+WARD_CRIME_01_10_OLDWARD_HASH = "cd9053db13b735ed5ddafb81a282e5b3e2aab66c"
+WARD_CRIME_10_21_HASH = "e8778ba783691cffaaa85171c554538c849baf72"
+WARD_CRIME_21_23_HASH = "3db0bc2f73f95aba7c739d827145a4c85cf6d160"
 
 def test_data_hash():
     """ Test that the data hash is correct"""
     assert setup.get_data_hash("LSOA_Crime_21-23.csv") == LSOA_CRIME_21_23_HASH
+    assert setup.get_data_hash("Borough_Crime_10-21.csv") == BOROUGH_CRIME_10_21_HASH
+    assert setup.get_data_hash("Borough_Crime_21-23.csv") == BOROUGH_CRIME_21_23_HASH
+    assert setup.get_data_hash("LSOA_Crime_10-21.csv") == LSOA_CRIME_10_21_HASH
+    assert setup.get_data_hash("Ward_Crime_01-10_OLDWARD.csv") == WARD_CRIME_01_10_OLDWARD_HASH
+    assert setup.get_data_hash("Ward_Crime_10-21.csv") == WARD_CRIME_10_21_HASH
+    assert setup.get_data_hash("Ward_Crime_21-23.csv") == WARD_CRIME_21_23_HASH
