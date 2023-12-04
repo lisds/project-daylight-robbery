b390cee63cec897ca1ce4e3bb086c43ef2ffc220
Pal Patel <pal.patel@lis.ac.uk>
2023-12-04 15:45:19 +0000

    Pushing the column name changes with simplified code, and starting to examine the columns

--- projtools/clean.py
+++ projtools/clean.py
@@ -3,26 +3,32 @@ from data import Data # pylint: disable=import-error disable=no-name-in-module
 
 data = Data()
 
-borough_10_21 = data.borough_crime_10_21
-borough_10_21 = borough_10_21.rename(columns={"MajorText": "Category",
-                               "MinorText": "Offence",
-                               "LookUp_BoroughName": "Borough"}) #renaming columns to make them more legible
-borough_21_23 = data.borough_crime_21_23
-borough_21_23 = borough_21_23.rename(columns={"MajorText": "Category",
-                               "MinorText": "Offence",
-                               "LookUp_BoroughName": "Borough"}) #renaming columns to make them more legible
-lsoa_10_21 = data.lsoa_crime_10_21
-lsoa_10_21 = lsoa_10_21.rename(columns={"Major Category": "Category",
-                                        "Minor Category": "Offence"}) #renaming columns to make them more legible
-lsoa_21_23 = data.lsoa_crime_21_23
-lsoa_21_23 = lsoa_21_23.rename(columns={"Major Category": "Category",
-                                        "Minor Category": "Offence"}) #renaming columns to make them more legible
-ward_10_21 = data.ward_crime_10_21
-ward_10_21 = ward_10_21.rename(columns={"MajorText": "Category",
-                                        "MinorText": "Offence",
-                                        "LookUp_BoroughName": "Borough"}) #renaming columns to make them more legible
-ward_21_23 = data.ward_crime_21_23
-ward_21_23 = ward_21_23.rename(columns={"MajorText": "Category",
-                                        "MinorText": "Offence",
-                                        "LookUp_BoroughName": "Borough"}) #renaming columns to make them more legible
+#renaming columns to make them more legible
+column_names = {"MajorText": "Category",
+                "MinorText": "Offence",
+                "LookUp_BoroughName": "Borough",
+                "Major Category": "Category",
+                "Minor Category": "Offence"}
 
+borough_10_21 = data.borough_crime_10_21
+borough_10_21 = borough_10_21.rename(columns=column_names)
+
+borough_21_23 = data.borough_crime_21_23
+borough_21_23 = borough_21_23.rename(columns=column_names)
+
+lsoa_10_21 = data.lsoa_crime_10_21
+lsoa_10_21 = lsoa_10_21.rename(columns=column_names)
+
+lsoa_21_23 = data.lsoa_crime_21_23
+lsoa_21_23 = lsoa_21_23.rename(columns=column_names)
+
+ward_10_21 = data.ward_crime_10_21
+ward_10_21 = ward_10_21.rename(columns=column_names)
+
+ward_21_23 = data.ward_crime_21_23
+ward_21_23 = ward_21_23.rename(columns=column_names)
+
+#checking clumn names are uniform/spelt properly
+print(borough_10_21.Category.value_counts())
+print(borough_10_21.Offence.value_counts())
+print(borough_10_21.Borough.unique())
