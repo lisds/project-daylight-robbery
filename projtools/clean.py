4b4fbe8587a3c7cdc4446a832e3d5e0c2d6df2f6
Tom Fox <13188249+TomWFox@users.noreply.github.com>
2023-12-04 20:28:38 +0000

    change file naming and refactor cleaning code

--- projtools/clean.py
+++ projtools/clean.py
@@ -4,27 +4,17 @@ from data import Data # pylint: disable=no-name-in-module
 data = Data()
 
 #renaming columns to make them more legible
-column_names = {"MajorText": "Category",
-                "MinorText": "Offence",
-                "LookUp_BoroughName": "Borough",
-                "Major Category": "Category",
-                "Minor Category": "Offence"}
+column_names = {"MajorText": "category",
+                "MinorText": "offence",
+                "LookUp_BoroughName": "borough",
+                "Major Category": "category",
+                "Minor Category": "offence"}
 
-borough_10_21 = data.borough_crime_10_21
-borough_10_21 = borough_10_21.rename(columns=column_names)
+borough_10_21 = data.borough_10_21.rename(columns=column_names)
+borough_21_23 = data.borough_21_23.rename(columns=column_names)
 
-borough_21_23 = data.borough_crime_21_23
-borough_21_23 = borough_21_23.rename(columns=column_names)
+lsoa_10_21 = data.lsoa_10_21.rename(columns=column_names)
+lsoa_21_23 = data.lsoa_21_23.rename(columns=column_names)
 
-lsoa_10_21 = data.lsoa_crime_10_21
-lsoa_10_21 = lsoa_10_21.rename(columns=column_names)
-
-lsoa_21_23 = data.lsoa_crime_21_23
-lsoa_21_23 = lsoa_21_23.rename(columns=column_names)
-
-ward_10_21 = data.ward_crime_10_21
-ward_10_21 = ward_10_21.rename(columns=column_names)
-
-ward_21_23 = data.ward_crime_21_23
-ward_21_23 = ward_21_23.rename(columns=column_names)
-
+ward_10_21 = data.ward_10_21.rename(columns=column_names)
+ward_21_23 = data.ward_21_23.rename(columns=column_names)
--- projtools/tests/test_setup.py
+++ projtools/tests/test_setup.py
@@ -3,20 +3,20 @@
 
 from projtools import setup
 
-BOROUGH_CRIME_10_21_HASH = "bcc488bec69c7a6ce7148dd5a05a025498c9488b"
-BOROUGH_CRIME_21_23_HASH = "ea7e1aa0dd0562ba2cc7ef8beffd1911f3c803f7"
-LSOA_CRIME_10_21_HASH = "f250f870067cd90a82b7acefbc3f7ccb2e765166"
-LSOA_CRIME_21_23_HASH = "8057ba79256769903b91b084e1ed0affd8e85277"
-WARD_CRIME_01_10_OLDWARD_HASH = "cd9053db13b735ed5ddafb81a282e5b3e2aab66c"
-WARD_CRIME_10_21_HASH = "e8778ba783691cffaaa85171c554538c849baf72"
-WARD_CRIME_21_23_HASH = "3db0bc2f73f95aba7c739d827145a4c85cf6d160"
+BOROUGH_10_21_HASH = "bcc488bec69c7a6ce7148dd5a05a025498c9488b"
+BOROUGH_21_23_HASH = "ea7e1aa0dd0562ba2cc7ef8beffd1911f3c803f7"
+LSOA_10_21_HASH = "f250f870067cd90a82b7acefbc3f7ccb2e765166"
+LSOA_21_23_HASH = "8057ba79256769903b91b084e1ed0affd8e85277"
+WARD_01_10_OLDWARD_HASH = "cd9053db13b735ed5ddafb81a282e5b3e2aab66c"
+WARD_10_21_HASH = "e8778ba783691cffaaa85171c554538c849baf72"
+WARD_21_23_HASH = "3db0bc2f73f95aba7c739d827145a4c85cf6d160"
 
 def test_data_hash():
     """ Test that the data hash is correct"""
-    assert setup.get_data_hash("lsoa_crime_21_23.csv") == LSOA_CRIME_21_23_HASH
-    assert setup.get_data_hash("borough_crime_10_21.csv") == BOROUGH_CRIME_10_21_HASH
-    assert setup.get_data_hash("borough_crime_21_23.csv") == BOROUGH_CRIME_21_23_HASH
-    assert setup.get_data_hash("lsoa_crime_10_21.csv") == LSOA_CRIME_10_21_HASH
-    assert setup.get_data_hash("ward_crime_01_10_oldward.csv") == WARD_CRIME_01_10_OLDWARD_HASH
-    assert setup.get_data_hash("ward_crime_10_21.csv") == WARD_CRIME_10_21_HASH
-    assert setup.get_data_hash("ward_crime_21_23.csv") == WARD_CRIME_21_23_HASH
+    assert setup.get_data_hash("lsoa_21_23.csv") == LSOA_21_23_HASH
+    assert setup.get_data_hash("borough_10_21.csv") == BOROUGH_10_21_HASH
+    assert setup.get_data_hash("borough_21_23.csv") == BOROUGH_21_23_HASH
+    assert setup.get_data_hash("lsoa_10_21.csv") == LSOA_10_21_HASH
+    assert setup.get_data_hash("ward_01_10_oldward.csv") == WARD_01_10_OLDWARD_HASH
+    assert setup.get_data_hash("ward_10_21.csv") == WARD_10_21_HASH
+    assert setup.get_data_hash("ward_21_23.csv") == WARD_21_23_HASH
