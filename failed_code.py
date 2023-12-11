4d6e779455894e1d83c6c78c9b5b09877351fffe
Pal Patel <pal.patel@lis.ac.uk>
2023-12-11 16:52:21 +0000

    added prompt in file

--- failed_code.py
+++ failed_code.py
@@ -1,1 +1,5 @@
-#this file is for failed/analysis attempts that were abandoned
+#this file is for failed/unsuccessful analysis attempts that were abandoned
+
+#Copy your code and explain what was attempted
+
+#explain why you were not successful or if you didn't find anything interesting
