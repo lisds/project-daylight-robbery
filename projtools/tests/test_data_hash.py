df3cf42e110013277efd538c97b3733510ec78bd
Pal Patel <pal.patel@lis.ac.uk>
2023-12-17 18:49:21 +0000

    updated datasets and changed hash values for them

--- projtools/tests/test_data_hash.py
+++ projtools/tests/test_data_hash.py
@@ -4,11 +4,11 @@
 from projtools import data_hash
 
 BOROUGH_10_21_HASH = "bcc488bec69c7a6ce7148dd5a05a025498c9488b"
-BOROUGH_21_23_HASH = "ea7e1aa0dd0562ba2cc7ef8beffd1911f3c803f7"
+BOROUGH_21_23_HASH = "4d702ed26a1291cdd1f12a62da2a04b4cba8cfe9"
 LSOA_10_21_HASH = "f250f870067cd90a82b7acefbc3f7ccb2e765166"
-LSOA_21_23_HASH = "8057ba79256769903b91b084e1ed0affd8e85277"
+LSOA_21_23_HASH = "5f17d817d45d63efdf776df87908b2f8ecc8f5a5"
 WARD_10_21_HASH = "e8778ba783691cffaaa85171c554538c849baf72"
-WARD_21_23_HASH = "3db0bc2f73f95aba7c739d827145a4c85cf6d160"
+WARD_21_23_HASH = "018daac5cb312f8b7c628f98d689ab0f3ce43820"
 
 def test_data_hash():
     """ Test that the data hash is correct"""
